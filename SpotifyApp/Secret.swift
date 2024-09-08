7f30176e13b961c551d9c7bb6a526d73626dbcac
Vladimir Fibe <motiw@icloud.com>
2024-09-08 21:25:10 +0500

    Delete SpotifyApp/Secret.swift

--- SpotifyApp/Secret.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-
-extension AuthManager {
-    struct Constants {
-        static let clientID = "d4863882d4704bbeb70fce2f385d42f4"
-        static let clientSecret = "7a10b92146e84207a92b2c2b8777c6bf"
-    }
-}
