5dba4131c0220592634a60538ec13c9608e4c2e7
Vladimir Fibe <motiw@icloud.com>
2024-09-08 13:22:53 +0500

    Initial Commit

---
+++ SpotifyApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SpotifyApp
+//
+//  Created by MacUser on 08.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
